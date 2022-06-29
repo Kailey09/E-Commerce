0050775e656866b04f4e4c06d4da83193c4e5ff2
Kailey09 <kcoop1323@gmail.com>
2022-06-29 17:21:32 -0400

    added delete route to delete category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -73,7 +73,19 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+  })
+  .then(function(category) {
+    res.json(category)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
